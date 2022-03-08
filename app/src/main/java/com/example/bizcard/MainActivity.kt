051de5f01c912b2524efa690a034dc306813ff66
dauni6 <45280927+dauni6@users.noreply.github.com>
2022-03-09 00:27:21 +0900

    parameter with Modifier
    Modifier를 왜 Composable 함수에 집어넣나 했더니, 아주 마법같이 동작한다.
    동일한 Composable 함수이지만 원하는대로 Modifier를 만들어 넣어주니, 고대로 Compose가 만들어진다. 이래서 parameter로 Modifier를 전달하는구나 느꼈다,

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
@@ -33,6 +44,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     // Surface는 Canvas라고 생각하면 된다. 메인 백그라운드가 된다.
     Surface(
         modifier = Modifier
@@ -42,20 +56,158 @@ fun CreateBizCard() {
             modifier = Modifier
                 .width(200.dp)
                 .height(390.dp)
-                .padding(12.dp)
-            ,
+                .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 4.dp,
         ) {
+
+            Column(
+                modifier = Modifier
+                    .height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }
+                ) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.button)
+                }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box {
+
+                    }
+                }
+            }
 
         }
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(
+                data = listOf(
+                    "Project 1",
+                    "Project 2",
+                    "Project 3",
+                    "Project 4",
+                    "Project 5"
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth()
+                    .clickable {
+
+                    },
+                shape = RectangleShape,
+                elevation = 4.dp,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(7.dp)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great Project", style = MaterialTheme.typography.body2)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PortfolioItem() {
+
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
+        Text(
+            text = "Miles P.",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant
+        )
+        Text(
+            text = "Android Compose Programmer",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "@themilesCompose",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+    }
+}
+
+@Composable
+fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_people_outline_24),
+            contentDescription = "profile",
+            modifier = modifier
+                .size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BizCardTheme {
-       CreateBizCard()
+        CreateBizCard()
     }
 }
